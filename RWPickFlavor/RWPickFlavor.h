a754080ee1e85008a7e858a2c2f5228a05fae3ef
startiis103@gmail.com <app@htaiwans-MacBook-Pro-2.local>
2015-06-27 11:01:07 +0800

    Initial commit

---
+++ RWPickFlavor/RWPickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  RWPickFlavor.h
+//  RWPickFlavor
+//
+//  Created by htaiwan on 6/27/15.
+//  Copyright (c) 2015 htaiwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RWPickFlavor.
+FOUNDATION_EXPORT double RWPickFlavorVersionNumber;
+
+//! Project version string for RWPickFlavor.
+FOUNDATION_EXPORT const unsigned char RWPickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RWPickFlavor/PublicHeader.h>
+
+
